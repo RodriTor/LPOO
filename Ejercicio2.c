acdd547201135e2abd39759065c1b2e70ce6e093
RodriTor <rodrigotorrez461@gmail.com>
2025-04-02 16:18:22 -0300

    Create Ejercicio2.c

---
+++ Ejercicio2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int i, total, contador=0;
+char cadena[100];
+main(){
+printf("Ingrese una cadena de caracteres: ");
+scanf("%s", cadena);
+
+for(i=0; i< strlen(cadena); i++){
+if(cadena[i] == 'a' || cadena[i] == 'e' || cadena[i] == 'i' || cadena[i] == 'o' || cadena[i] == 'u' ||
+   cadena[i] == 'A' || cadena[i] == 'E' || cadena[i] == 'I' || cadena[i] == 'O' || cadena[i] == 'U' ){
+   contador++;  
+}
+}
+  printf("El numero de vocales son: %s", contador);
+}
