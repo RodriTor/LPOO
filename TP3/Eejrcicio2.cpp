8f7f3f3b500bb78b20783bffb62207ec7f47c361
RodriTor <rodrigotorrez461@gmail.com>
2025-04-15 20:39:33 -0300

    Eejrcicio2.cpp
    terminao

--- TP3/Eejrcicio2.cpp
+++ TP3/Eejrcicio2.cpp
@@ -1,37 +1,28 @@
 #include <stdio.h>
-
-void calcularSumaYPromedio(int *arr, int tam, int *suma, float *promedio) {
-    *suma = 0; // Inicializamos la suma en 0
-
-
-    for (int i = 0; i < tam; i++) {
-        *suma += *(arr + i); 
-    }
-
-    *promedio = (float)(*suma) / tam;
+//2. Encontrar la suma y el promedio de los elementos de un arreglo usando punteros y funciones.
+void SumaYPromedio (int *suma, int *promedio, int tam, int *arreglo){
+	*suma = 0;
+	
+	for(int i=0; i < tam; i++){
+	*suma +=  *(arreglo + i);
 }
-
-int main() {
-    int tam;
-
-    printf("Ingrese la cantidad de elementos del arreglo: ");
-    scanf("%d", &tam);
-
-    int arreglo[tam];
-
-
-    for (int i = 0; i < tam; i++) {
-        printf("Ingrese el elemento %d: ", i + 1);
-        scanf("%d", &arreglo[i]);
-    }
-
-    int suma;
-    float promedio;
-
-
-    calcularSumaYPromedio(arreglo, tam, &suma, &promedio);
-    printf("\nLa suma de los elementos es: %d\n", suma);
-    printf("El promedio de los elementos es: %.2f\n", promedio);
-
-    return 0;
+	*promedio = (*suma)/tam;
+	
+}
+main(){
+	int tam, suma, promedio;
+	printf("Ingrese el tamaño del arreglo: ");
+	scanf("%d", &tam);
+	
+	int arreglo[tam];
+	
+	for(int i=0; i < tam; i++){
+		printf("Ingrese numero: ");
+		scanf("%d", &arreglo[i]);
+	}
+	
+	SumaYPromedio(&suma, &promedio, tam, arreglo);
+	
+	printf("La suma de todos los elementos es: %d\n", suma);
+	printf("El promedio de todos los elementos es %d\n", promedio);
 }
