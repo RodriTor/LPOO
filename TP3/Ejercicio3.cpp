c4489db43eb3ff1a7587c3a7c7619b9f03ede677
RodriTor <rodrigotorrez461@gmail.com>
2025-04-15 21:14:31 -0300

    Ejercicio3.cpp
    !!

--- TP3/Ejercicio3.cpp
+++ TP3/Ejercicio3.cpp
@@ -1,42 +1,35 @@
 #include <stdio.h>
-
-void buscarMayorYMenor(int *arr, int tam, int *mayor, int *menor) {
- 
-    *mayor = *arr;
-    *menor = *arr;
-
-
-    for (int i = 1; i < tam; i++) {
-        if (*(arr + i) > *mayor) {
-            *mayor = *(arr + i);
-        }
-        if (*(arr + i) < *menor) {
-            *menor = *(arr + i);
-        }
-    }
+// 3. Encontrar el mayor y el menor elemento de un arreglo usando punteros y funciones.
+void MayorYMenor(int *arreglo, int *mayor, int *menor, int tam){
+	*mayor = *arreglo;
+	*menor = *arreglo;
+	
+	for(int i=0; i<tam; i++){
+		if(*(arreglo + i) > *mayor){
+			*mayor = *(arreglo +i);
+		}
+		if(*(arreglo + i) < *menor){
+			*menor = *(arreglo +i);
+		}
+	}
+	
 }
-
-int main() {
-    int tam;
-
-    printf("Ingrese la cantidad de elementos: ");
-    scanf("%d", &tam);
-
-    int arreglo[tam];
-
-    for (int i = 0; i < tam; i++) {
-        printf("Elemento %d: ", i + 1);
-        scanf("%d", &arreglo[i]);
-    }
-
-
-    int mayor, menor;
-
-    buscarMayorYMenor(arreglo, tam, &mayor, &menor);
-
-
-    printf("\nEl valor mayor es: %d\n", mayor);
-    printf("El valor menor es: %d\n", menor);
-
-    return 0;
+main(){
+	int tam;
+	int mayor, menor;
+	
+	printf("Ingrese el tamaño del arreglo: ");
+	scanf("%d", &tam);
+	
+	int arreglo[tam];
+	 
+	for(int i=0; i<tam; i++){
+		printf("Ingrese elemento: ");
+		scanf("%d", &arreglo[i]);
+	}
+	
+	MayorYMenor(arreglo, &mayor, &menor, tam);
+	
+	printf("El elemento mayor del arreglo es: %d\n", mayor);
+	printf("El elemento menor del arreglo es: %d", menor);
 }
