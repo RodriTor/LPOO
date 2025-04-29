b646dc89b50d55dd5955362238ed955aacbd12d2
RodriTor <rodrigotorrez461@gmail.com>
2025-04-29 16:09:11 -0300

    Ejercicio1.cpp
    ...

--- TP2/Ejercicio1.cpp
+++ TP2/Ejercicio1.cpp
@@ -1,5 +1,6 @@
 
 PUNTO 1-TP2 
+	
 #include <stdio.h>
 char nombre[99];
 int main(){
@@ -7,3 +8,42 @@ int main(){
 	scanf("%s", nombre);
 	printf("Adivino!!, tu nombre es %s", nombre);
 }
+
+/*
+1. Pedir al usuario que ingrese su nombre y luego imprimirlo en pantalla.
+
+2. Realiza un programa que cuente cuántas vocales tiene una cadena de
+caracteres ingresada por el usuario.
+
+3. Realiza un programa que determine si una cadena de caracteres ingresada
+por el usuario es palíndromo (se lee igual de izquierda a derecha que de
+derecha a izquierda).
+
+4. Realiza un programa que reemplace todas las apariciones de un carácter
+en una cadena de caracteres ingresada por el usuario por otro carácter
+también ingresado por el usuario.
+
+5. Escribe un programa que cuente el número de palabras en una cadena
+ingresada por el usuario.
+6. Crea un programa que invierta el orden de las palabras en una cadena (por
+ejemplo, &quot;Hola mundo&quot; se convierte en &quot;mundo Hola&quot;).
+7. Realiza un programa que determine si una cadena de caracteres ingresada
+por el usuario es un anagrama de otra cadena también ingresada por el
+usuario. Un anagrama es una palabra o frase formada por las mismas
+letras de otra palabra o frase, pero en un orden diferente, como por
+ejemplo &quot;roma&quot; y &quot;amor&quot;.
+8. Realizar un programa que ingrese 5 palabras y devuelta la cadena de
+caracteres repetidos más larga entre ellas (si ingreso metro, metralleta Y
+metido debe devolver la cadena “met”)
+9. Realizar un programa en el cual el usuario primero ingrese una oración
+
+la cual puede estar en mayúscula o minúscula o de manera alternada
+y luego pueda seleccionar que realice las siguientes acciones
+a) Mostrar la oración toda en mayúscula
+b) Mostrar la oración todo en minúsculas
+c) Mostrar la oración alternando una mayúscula y una minúscula
+D) Mostrar la oración comenzando todas las palabras con mayúscula
+10. Desarrolla un programa que busque la primera aparición de una
+subcadena dentro de una cadena más larga. Mencionar si dicha subcadena
+se encuentra y en qué posición se empieza.
+*/
