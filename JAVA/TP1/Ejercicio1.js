03557f127feb35c5a414b53885a4e8dd87b19c6f
RodriTor <rodrigotorrez461@gmail.com>
2025-06-05 09:04:08 -0300

    Create Ejercicio1.js

---
+++ JAVA/TP1/Ejercicio1.js
@@ -0,0 +1,14 @@
+public class ClasePrimaria {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=100; i++) {			
+			if(i % 2 == 0) {
+				System.out.print("PAR ");
+			}
+			else {
+				System.out.println( i);
+			}
+			
+		}
+	}
+}
