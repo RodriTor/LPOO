c0774579cb4586f7fc7509d4c3b05a15f3144db1
RodriTor <rodrigotorrez461@gmail.com>
2025-06-10 18:32:32 -0300

    Create ejercicio3.java

---
+++ JAVA/ejercicio3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class ejercicio3 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int numero;
+		
+		System.out.println("Ingrese un numero: ");
+	    numero = scanner.nextInt();
+
+	    if(numero > 100) {
+	    	boolean esPrimo true;
+	        for (int j = 2; j <= i/2; j++) {
+	            if (i % j == 0) {
+	                esPrimo = false;
+	                break;
+	            }
+	        }
+	    }
+	}
+
+}
