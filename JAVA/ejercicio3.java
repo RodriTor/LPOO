01ea20edc746d53157d80f1c55b7cc61853551a3
RodriTor <rodrigotorrez461@gmail.com>
2025-06-11 08:33:01 -0300

    Update ejercicio3.java

--- JAVA/ejercicio3.java
+++ JAVA/ejercicio3.java
@@ -11,14 +11,14 @@ public class ejercicio3 {
 	    numero = scanner.nextInt();
 		divisores = 0;
 	    if(numero > 100) {
-	    	for(int i=1; i<numero; i++) {
+	    	for(int i=1; i<numero/2; i++) {
 	    		if(numero % i == 0) {
 	    			divisores++;
 	    		}
 	    	}
 	    }
 		
-		}while(numero < 100);
+		}while(numero < 100 || divisores > 2);
 		
 		System.out.println(numero + " Es mayor que 100 y es primo");
 	}
