06220b7728cc628936c53669ffc34db445493e0c
RodriTor <rodrigotorrez461@gmail.com>
2025-06-17 17:26:11 -0300

    Update ejercicio5.java

--- JAVA/ejercicio5.java
+++ JAVA/ejercicio5.java
@@ -7,14 +7,15 @@ public class ejercicio5 {
 		int numero;
 		
 		System.out.println("Ingrese un numero (1 al 10): ");
-	    numero = scanner.nextInt();
+	    numero = scanner.nextInt(); // Lee el número para generar su tabla
 
 	    System.out.println("La tabla de multiplicar del numero " + numero + " es: ");
-	    for(int i=1; i<=10; i++) {
-	    	int multi = numero * i;
-	    	System.out.println(i + "x" + numero + " = " + multi ); 
+	    
+	    for(int i = 1; i <= 10; i++) { // Recorre los valores del 1 al 10
+	    	int multi = numero * i; // Calcula la multiplicación actual
+	    	System.out.println(i + "x" + numero + " = " + multi); // Imprime el resultado en formato de tabla
 	    }
-	    scanner.close();
-	}
 
+	    scanner.close(); // Cierra el objeto scanner
+	}
 }
