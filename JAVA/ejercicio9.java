4196613895980003cd85c41a7e20d86aacc9e6dc
RodriTor <rodrigotorrez461@gmail.com>
2025-06-17 17:44:51 -0300

    Update ejercicio9.java

--- JAVA/ejercicio9.java
+++ JAVA/ejercicio9.java
@@ -6,23 +6,28 @@ public class ejercicio9 {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Ingrese el primer numero:");
-        int num1 = scanner.nextInt();
+        int num1 = scanner.nextInt(); // Lee el primer número
 
         System.out.println("Ingrese el segundo numero:");
-        int num2 = scanner.nextInt();
+        int num2 = scanner.nextInt(); // Lee el segundo número
 
         System.out.println("Ingrese el tercer numero:");
-        int num3 = scanner.nextInt();
+        int num3 = scanner.nextInt(); // Lee el tercer número
 
-        int mayor = num1;
+        int mayor = num1; // Asume inicialmente que el primero es el mayor
 
-        if (num2 > mayor) {
-            mayor = num2;
+        if (num2 > mayor) { // Compara con el segundo número
+            mayor = num2; // Actualiza si el segundo es mayor
         }
 
-        if (num3 > mayor) {
-            mayor = num3;
+        if (num3 > mayor) { // Compara con el tercer número
+            mayor = num3; // Actualiza si el tercero es mayor
         }
+
+        System.out.println("El número mayor es: " + mayor); // Muestra el mayor número
+        scanner.close(); // Cierra el scanner
+    }
+}
 
         System.out.println("El número mayor es: " + mayor);
         scanner.close();
