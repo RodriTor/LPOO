a7a51935a216e80f0937c7423eb68c54534cca9f
RodriTor <rodrigotorrez461@gmail.com>
2025-06-17 17:21:06 -0300

    Update Ejercicio1.java

--- JAVA/Ejercicio1.java
+++ JAVA/Ejercicio1.java
@@ -1,17 +1,15 @@
+public class ClasePrimaria { // Define una clase pública llamada ClasePrimaria
 
-public class ClasePrimaria {
-
-	public static void main(String[] args) {
-		for(int i=1; i<=100; i++) {			
-			if(i % 2 == 0) {
-				System.out.println("PAR ");
+	public static void main(String[] args) { // Método principal donde inicia el programa
+		for(int i = 1; i <= 100; i++) { // Bucle que recorre los números del 1 al 100
+			
+			if(i % 2 == 0) { // Si el número es divisible por 2 (es par)
+				System.out.println("PAR "); // Imprime la palabra "PAR"
 			}
-			else {
-				System.out.println( i);
+			else { // Si el número no es par (es impar)
+				System.out.println(i); // Imprime el número impar
 			}
 			
 		}
 	}
 }
-
-
